b708d601cf6de8f1988b89bab10064e012499fb0
MityaAndreevich <dmitry.logachev@icloud.com>
2021-11-04 19:45:05 +0300

    Initial Commit

---
+++ StateAndDataFlowInSwiftUI/StateAndDataFlowInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowInSwiftUIApp.swift
+//  StateAndDataFlowInSwiftUI
+//
+//  Created by Dmitry Logachev on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
